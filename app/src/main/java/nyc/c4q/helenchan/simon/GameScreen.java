7d4ecd8d403050ca4f8f7cad904a541a7ce39eee
Helen Chan <hchan22@gmail.com>
2016-10-22 23:52:44 -0400

    bug fixes,sounds added, fixed flashing

--- app/src/main/java/nyc/c4q/helenchan/simon/GameScreen.java
+++ app/src/main/java/nyc/c4q/helenchan/simon/GameScreen.java
@@ -1,14 +1,17 @@
 package nyc.c4q.helenchan.simon;
 
 import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,20 +25,22 @@ import java.util.Random;
 public class GameScreen extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "hi";
-    private static final long ONE_SECOND = 700;
+    private static final long ONE_SECOND = 1000;
     private Button redButton;
     private Button blueButton;
     private Button greenButton;
     private Button yellowButton;
     public int arrayNumberValues;
     public TextView roundNumber;
-    public long TWO_SECONDS = 2000;
     public int level = 1;
-
+    private int delayRound;
+    private static boolean checkingForMatch;
 
     Map<Integer, String> colorGen = new HashMap<Integer, String>();
     ArrayList<Integer> ranNum = new ArrayList<>();
     ArrayList<Integer> userInput = new ArrayList<>();
+
+
 
 
     @Override
@@ -65,7 +70,6 @@ public class GameScreen extends AppCompatActivity implements View.OnClickListene
 
 
     public int randomNumberGen() {
-        //int randomColorNum = (int) (Math.random() * 4);
         Random rand = new Random();
         int randomColorNum = rand.nextInt(4);
         Log.d(TAG, String.valueOf((randomColorNum)));
@@ -79,15 +83,14 @@ public class GameScreen extends AppCompatActivity implements View.OnClickListene
 
     }
 
-//
-//    public void listingArrayContents(ArrayList<Integer> array) {
-//        for (int i = 0; i < array.size(); i++) {
-//            arrayNumberValues = array.get(i);
-//        }
-//    }
 
 
     public void colorFlashInGame() {
+        final MediaPlayer buttonSoundRed = MediaPlayer.create(this, R.raw.sound_one);
+        final MediaPlayer buttonSoundYellow = MediaPlayer.create(this, R.raw.sound_two);
+        final MediaPlayer buttonSoundBlue = MediaPlayer.create(this, R.raw.sound_three);
+        final MediaPlayer buttonSoundGreen = MediaPlayer.create(this, R.raw.sound_four);
+
         for (int i = 0; i < ranNum.size(); i++) {
             final Handler handle = new Handler();
             final int finalI = i;
@@ -98,49 +101,55 @@ public class GameScreen extends AppCompatActivity implements View.OnClickListene
 
             String selectedColor = colorGen.get(arrayNumberValues);
             Log.d(TAG, selectedColor);
+
             switch (selectedColor) {
                 case "green":
                     handle.postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            greenButton.setBackgroundColor(getResources().getColor(R.color.clickgreen));
+                            flashGreen();
+                            buttonSoundGreen.start();
                         }
-                    }, ONE_SECOND);
+                    }, (ONE_SECOND * finalI)  );
                     break;
+
                 case "red":
+//                    redButton.setBackgroundColor(getResources().getColor(R.color.clickred));
                     handle.postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            redButton.setBackgroundColor(getResources().getColor(R.color.clickred));
+                                flashRed();
+                             buttonSoundRed.start();
+//                            resetButtonColors();
                         }
-                    }, ONE_SECOND);
+                    }, (ONE_SECOND * finalI)  );
                     break;
                 case "blue":
+//                    blueButton.setBackgroundColor(getResources().getColor(R.color.clickblue));
                     handle.postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            blueButton.setBackgroundColor(getResources().getColor(R.color.clickblue));
+                            flashBlue();
+                            buttonSoundBlue.start();
+//                            resetButtonColors();
                         }
-                    }, ONE_SECOND);
+                    }, (ONE_SECOND * finalI)  );
                     break;
                 case "yellow":
+//                    yellowButton.setBackgroundColor(getResources().getColor(R.color.clickyellow));
                     handle.postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            yellowButton.setBackgroundColor(getResources().getColor(R.color.clickyellow));
+                        flashYellow();
+                            buttonSoundYellow.start();
+//                            resetButtonColors();
                         }
-                    }, ONE_SECOND);
+                    }, (ONE_SECOND * finalI)  );
                     break;
             }
                 }
-            }, 200);
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    resetButtonColors();
-                }
-            }, TWO_SECONDS);
+            }, delayRound +=250);
+
         }
     }
 
@@ -155,17 +164,17 @@ public class GameScreen extends AppCompatActivity implements View.OnClickListene
 
     public boolean areArraysEqual(ArrayList input, ArrayList setList) {
         if (setList.size() != input.size()) {
-            return false;
-
-        } else {
-            for (int j = 0; j < input.size(); j++) {
+           for (int j = 0; j < input.size(); j++) {
                 if (setList.get(j) != input.get(j))
                     return false;
             }
         }
+        Log.d(TAG," AREARRAYSEQUAL this is going to be true");
         return true;
-
     }
+
+
+
 
     public void roundWon() {
         userInput.clear();
@@ -176,62 +185,90 @@ public class GameScreen extends AppCompatActivity implements View.OnClickListene
 
     @Override
     public void onClick(View view) {
+        MediaPlayer buttonSoundRed = MediaPlayer.create(this, R.raw.sound_one);
+        MediaPlayer buttonSoundYellow = MediaPlayer.create(this, R.raw.sound_two);
+        MediaPlayer buttonSoundBlue = MediaPlayer.create(this, R.raw.sound_three);
+        MediaPlayer buttonSoundGreen = MediaPlayer.create(this, R.raw.sound_four);
+
         switch (view.getId()) {
             case R.id.red:
                 userInput.add(0);
+                buttonSoundRed.start();
+                areArraysEqual(userInput, ranNum);
+
                 break;
             case R.id.yellow:
                 userInput.add(1);
+                buttonSoundYellow.start();
                 Log.i("RAN NUM ARRAY SIZE", ranNum.size() + "");
+                areArraysEqual(userInput, ranNum);
 
                 break;
             case R.id.blue:
                 userInput.add(2);
+                buttonSoundBlue.start();
+                areArraysEqual(userInput, ranNum);
+
                 break;
             case R.id.green:
                 userInput.add(3);
+                buttonSoundGreen.start();
+                areArraysEqual(userInput, ranNum);
                 break;
-
         }
         Log.d(TAG, "switching");
 
-        if (areArraysEqual(userInput, ranNum)) {
+
+        if (areArraysEqual(userInput, ranNum) == true) {
             level++;
             roundNumber.setText("Round: " + String.valueOf(level));
             roundWon();
 
-        } else {
-            Toast.makeText(GameScreen.this, "You Lose", Toast.LENGTH_LONG);
-
+        } else if (areArraysEqual(userInput, ranNum) == false) {
+//            Toast.makeText(GameScreen.this, "You Lose", Toast.LENGTH_LONG).show();
+//            finish();
         }
-
     }
 
+    private void flashRed() {
+        Log.d(TAG,"RED_ANIMATION");
+        Animation myAnimation = new AlphaAnimation(1,0);
+        myAnimation.setDuration(100);
+        myAnimation.setInterpolator(new LinearInterpolator());
+        myAnimation.setRepeatCount(1);
+        myAnimation.setRepeatMode(Animation.REVERSE);
+        redButton.startAnimation(myAnimation);
+    }
+
+    private void flashBlue() {
+        Log.d(TAG,"BLUE_ANIMATION");
+        Animation myAnimation = new AlphaAnimation(1, 0);
+        myAnimation.setDuration(100);
+        myAnimation.setInterpolator(new LinearInterpolator());
+        myAnimation.setRepeatCount(1);
+        myAnimation.setRepeatMode(Animation.REVERSE);
+        blueButton.startAnimation(myAnimation);
+    }
+
+    private void flashGreen() {
+        Log.d(TAG,"GREEN_ANIMATION");
+        Animation myAnimation = new AlphaAnimation(1, 0);
+        myAnimation.setDuration(100);
+        myAnimation.setInterpolator(new LinearInterpolator());
+        myAnimation.setRepeatCount(1);
+        myAnimation.setRepeatMode(Animation.REVERSE);
+        greenButton.startAnimation(myAnimation);
+    }
+    private void flashYellow() {
+        Log.d(TAG,"YELLOW_ANIMATION");
+        Animation myAnimation = new AlphaAnimation(1, 0); //transparency
+        myAnimation.setDuration(100); //how long to change color
+        myAnimation.setInterpolator(new LinearInterpolator());  //rate of change
+        myAnimation.setRepeatCount(1);  //change color once
+        myAnimation.setRepeatMode(Animation.REVERSE); //change back to original color
+        yellowButton.startAnimation(myAnimation); //to start animation method
+    }
 
 }
 
 
-//    public void addUserInputToArr(){
-//        userInput.add();
-//    }
-
-//    public void buttonClick(View v){
-//        switch (v.getId()){
-//            case R.id.blue:
-//
-//        }
-//    }
-//    public void getColor(){
-//        for (Integer random:colorGen.keySet()){
-//            colorGen.get(random);
-//        }
-//    }
-
-//    Handler handler = new Handler();
-//    handler.postDelayed(new Runnable() {
-//        @Override
-//        public void run() {
-//            // Do something after 5s = 5000ms
-//        }
-//    }, 5000);
-//    colorGen.get(randomNumberGen())
